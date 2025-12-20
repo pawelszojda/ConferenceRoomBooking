7f85045ee8d93fa1e070259ae54fea6a00a82b9a
Paweł Szojda <pawelszojda@icloud.com>
2025-12-20 17:58:26 +0100

    Add AppUser and Reservation models with properties and relationships

--- obj/Debug/net9.0/ConferenceRoomBooking.AssemblyInfo.cs
+++ obj/Debug/net9.0/ConferenceRoomBooking.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConferenceRoomBooking")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34334b20714f0a8789866eda0a7083463ee403e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConferenceRoomBooking")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConferenceRoomBooking")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
