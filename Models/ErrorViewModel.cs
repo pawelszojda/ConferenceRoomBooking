34334b20714f0a8789866eda0a7083463ee403e8
Paweł Szojda <pawelszojda@icloud.com>
2025-12-20 17:56:18 +0100

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ConferenceRoomBooking.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
